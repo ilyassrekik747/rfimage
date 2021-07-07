ef50407d237917c83e77f0a9cd745c7d9093bc06
ilyassrekik747 <43963531+ilyassrekik747@users.noreply.github.com>
2021-07-07 15:43:17 +0400

    Add files via upload

---
+++ DATA.py
@@ -0,0 +1,808 @@
+az=b'000\n'
+a1=b'001\n'
+a2=b'002\n'
+a3=b'003\n'
+a4=b'004\n'
+a5=b'005\n'
+a6=b'006\n'
+a7=b'007\n'
+a8=b'008\n'
+a9=b'009\n'
+a10=b'010\n'
+a11=b'011\n'
+a12=b'012\n'
+a13=b'013\n'
+a14=b'014\n'
+a15=b'015\n'
+a16=b'016\n'
+a17=b'017\n'
+a18=b'018\n'
+a19=b'019\n'
+a20=b'020\n'
+a21=b'021\n'
+
+a22=b'021\n'
+a23=b'023\n'
+a24=b'024\n'
+a25=b'025\n'
+a26=b'026\n'
+a27=b'027\n'
+a28=b'028\n'
+a29=b'029\n'
+a30=b'030\n'
+a31=b'031\n'
+a32=b'032\n'
+a33=b'033\n'
+a34=b'034\n'
+a35=b'035\n'
+a36=b'036\n'
+a37=b'037\n'
+a38=b'038\n'
+a39=b'039\n'
+a40=b'040\n'
+
+
+a41=b'041\n'
+
+a42=b'042\n'
+a43=b'043\n'
+a44=b'044\n'
+a45=b'045\n'
+a46=b'046\n'
+a47=b'047\n'
+a48=b'048\n'
+a49=b'049\n'
+a50=b'050\n'
+a51=b'051\n'
+a52=b'052\n'
+a53=b'053\n'
+a54=b'054\n'
+a55=b'055\n'
+a56=b'056\n'
+a57=b'057\n'
+a58=b'058\n'
+a59=b'059\n'
+a60=b'060\n'
+a61=b'061\n'
+
+a62=b'062\n'
+a63=b'063\n'
+a64=b'064\n'
+a65=b'065\n'
+a66=b'066\n'
+a67=b'067\n'
+a68=b'068\n'
+a69=b'069\n'
+a70=b'070\n'
+a71=b'071\n'
+
+a72=b'072\n'
+a73=b'073\n'
+a74=b'074\n'
+a75=b'075\n'
+a76=b'076\n'
+a77=b'077\n'
+a78=b'078\n'
+a79=b'079\n'
+a80=b'080\n'
+a81=b'081\n'
+
+a82=b'082\n'
+a83=b'083\n'
+a84=b'084\n'
+a85=b'085\n'
+a86=b'086\n'
+a87=b'087\n'
+a88=b'088\n'
+a89=b'089\n'
+
+a90=b'090\n'
+a91=b'091\n'
+
+a92=b'092\n'
+a93=b'093\n'
+a94=b'094\n'
+a95=b'095\n'
+a96=b'096\n'
+a97=b'097\n'
+a98=b'098\n'
+a99=b'099\n'
+
+a100=b'100\n'
+a101=b'101\n'
+
+a102=b'102\n'
+a103=b'103\n'
+a104=b'104\n'
+a105=b'105\n'
+a106=b'106\n'
+a107=b'107\n'
+a108=b'108\n'
+a109=b'109\n'
+
+a110=b'110\n'
+a111=b'111\n'
+
+a112=b'112\n'
+a113=b'113\n'
+a114=b'114\n'
+a115=b'115\n'
+a116=b'116\n'
+a117=b'117\n'
+a118=b'118\n'
+a119=b'119\n'
+a120=b'120\n'
+a121=b'121\n'
+
+a122=b'122\n'
+a123=b'123\n'
+a124=b'124\n'
+a125=b'125\n'
+a126=b'126\n'
+a127=b'127\n'
+a128=b'128\n'
+a129=b'129\n'
+a130=b'130\n'
+a131=b'131\n'
+
+a132=b'132\n'
+a133=b'133\n'
+a134=b'134\n'
+a135=b'135\n'
+a136=b'136\n'
+a137=b'137\n'
+a138=b'138\n'
+a139=b'139\n'
+
+a140=b'140\n'
+a141=b'141\n'
+
+a142=b'142\n'
+a143=b'143\n'
+a144=b'144\n'
+a145=b'145\n'
+a146=b'146\n'
+a147=b'147\n'
+a148=b'148\n'
+a149=b'149\n'
+
+a150=b'150\n'
+a151=b'151\n'
+
+a152=b'152\n'
+a153=b'153\n'
+a154=b'154\n'
+a155=b'155\n'
+a156=b'156\n'
+a157=b'157\n'
+a158=b'158\n'
+a159=b'159\n'
+
+a160=b'160\n'
+a161=b'161\n'
+
+a162=b'162\n'
+a163=b'163\n'
+a164=b'164\n'
+a165=b'165\n'
+a166=b'166\n'
+a167=b'167\n'
+a168=b'168\n'
+a169=b'169\n'
+
+a170=b'170\n'
+a171=b'171\n'
+
+a172=b'172\n'
+a173=b'173\n'
+a174=b'174\n'
+a175=b'175\n'
+a176=b'176\n'
+a177=b'177\n'
+a178=b'178\n'
+a179=b'179\n'
+
+a180=b'180\n'
+a181=b'181\n'
+
+a182=b'182\n'
+a183=b'183\n'
+a184=b'184\n'
+a185=b'185\n'
+a186=b'186\n'
+a187=b'187\n'
+a188=b'188\n'
+a189=b'189\n'
+
+a190=b'190\n'
+a191=b'191\n'
+
+a192=b'192\n'
+a193=b'193\n'
+a194=b'194\n'
+a195=b'195\n'
+a196=b'196\n'
+a197=b'197\n'
+a198=b'198\n'
+a199=b'199\n'
+
+a200=b'200\n'
+a201=b'201\n'
+
+a202=b'202\n'
+a203=b'203\n'
+a204=b'204\n'
+a205=b'205\n'
+a206=b'206\n'
+a207=b'207\n'
+a208=b'208\n'
+a209=b'209\n'
+
+
+a210=b'210\n'
+a211=b'211\n'
+
+a212=b'212\n'
+a213=b'213\n'
+a214=b'214\n'
+a215=b'215\n'
+a216=b'216\n'
+a217=b'217\n'
+a218=b'218\n'
+a219=b'219\n'
+
+a220=b'220\n'
+a221=b'221\n'
+
+a222=b'222\n'
+a223=b'223\n'
+a224=b'224\n'
+a225=b'225\n'
+a226=b'226\n'
+a227=b'227\n'
+a228=b'228\n'
+a229=b'229\n'
+
+a230=b'230\n'
+a231=b'231\n'
+a232=b'232\n'
+a233=b'233\n'
+a234=b'234\n'
+a235=b'235\n'
+a236=b'236\n'
+a237=b'237\n'
+a238=b'238\n'
+a239=b'239\n'
+a240=b'240\n'
+a241=b'241\n'
+a242=b'242\n'
+a243=b'243\n'
+a244=b'244\n'
+a245=b'245\n'
+a246=b'246\n'
+a247=b'247\n'
+a248=b'248\n'
+a249=b'249\n'
+a250=b'250\n'
+a251=b'251\n'
+a252=b'252\n'
+a253=b'253\n'
+a254=b'254\n'
+a255=b'255\n'
+
+def data(a):
+    if a==1:
+        return a1
+    elif a==2:
+        return a2
+    elif a==0:
+        return az
+    elif a==3:
+        return a3
+    elif a==4:
+        return a4
+    elif a==5:
+        return a5
+    elif a==6:
+        return a6
+    elif a==7:
+        return a7
+    elif a==8:
+        return a8
+    elif a==9:
+        return a9
+    elif a==10:
+        return a10
+    elif a==11:
+        return a11
+    elif a==12:
+        return a12
+    elif a==13:
+        return a13
+    elif a==14:
+        return a14
+    elif a==15:
+        return a15
+    elif a==16:
+        return a16
+    elif a==17:
+        return a17
+    elif a==18:
+        return a18
+    elif a==19:
+        return a19
+    elif a==20:
+        return a20
+    elif a==21:
+        return a21
+    elif a==22:
+        return a22
+    elif a==23:
+        return a23
+    elif a==24:
+        return a24
+    elif a==25:
+        return a25
+    elif a==26:
+        return a26
+    elif a==27:
+        return a27
+    elif a==28:
+        return a28
+    elif a==29:
+        return a29
+    elif a==30:
+        return a30
+    elif a==31:
+        return a31
+    elif a==32:
+        return a32
+    elif a==33:
+        return a33
+    elif a==34:
+        return a34
+    elif a==35:
+        return a35
+    elif a==36:
+        return a36
+
+    elif a==37:
+        return a37
+    elif a==38:
+        return a38
+    elif a==39:
+        return a39
+    elif a==40:
+        return a40
+    elif a==41:
+        return a41
+    elif a==42:
+        return a42
+    elif a==43:
+        return a43
+    elif a==44:
+        return a44
+    elif a==45:
+        return a45
+    elif a==46:
+        return a46
+    elif a==47:
+        return a47
+    elif a==48:
+        return a48
+    elif a==49:
+        return a49
+    elif a==50:
+        return a50
+    elif a==51:
+        return a51
+    elif a==52:
+        return a52
+    elif a==53:
+        return a53
+    elif a==54:
+        return a54
+    elif a==55:
+        return a55
+    elif a==56:
+        return a56
+    elif a==57:
+        return a57
+    elif a==58:
+        return a58
+    elif a==59:
+        return a59
+    elif a==60:
+        return a60
+    elif a==61:
+        return a61
+    elif a==62:
+        return a62
+    elif a==63:
+        return a63
+    elif a==64:
+        return a64
+    elif a==65:
+        return a65
+    elif a==66:
+        return a66
+    elif a==67:
+        return a67
+    elif a==68:
+        return a68
+    elif a==69:
+        return a69
+    elif a==70:
+        return a70
+    elif a == 71:
+        return a71
+    elif a == 72:
+        return a72
+    elif a == 73:
+        return a73
+    elif a == 74:
+        return a74
+    elif a == 75:
+        return a75
+    elif a == 76:
+        return a76
+    elif a == 77:
+        return a77
+    elif a == 78:
+        return a78
+    elif a == 79:
+        return a79
+    elif a == 80:
+        return a80
+    elif a == 81:
+        return a81
+    elif a == 82:
+        return a82
+    elif a == 83:
+        return a83
+    elif a == 84:
+        return a84
+    elif a == 85:
+        return a85
+    elif a == 86:
+        return a86
+    elif a == 87:
+        return a87
+    elif a == 88:
+        return a88
+    elif a == 89:
+        return a89
+    elif a == 90:
+        return a90
+    elif a == 91:
+        return a91
+    elif a == 92:
+        return a92
+    elif a == 93:
+        return a93
+    elif a == 94:
+        return a94
+    elif a == 95:
+        return a95
+    elif a == 96:
+        return a96
+    elif a == 97:
+        return a97
+    elif a == 98:
+        return a98
+    elif a == 99:
+        return a99
+    elif a == 100:
+        return a100
+    elif a == 101:
+        return a101
+    elif a == 102:
+        return a102
+    elif a == 103:
+        return a103
+    elif a == 104:
+        return a104
+    elif a == 105:
+        return a105
+    elif a == 106:
+        return a106
+    elif a == 107:
+        return a107
+    elif a == 108:
+        return a108
+    elif a == 109:
+        return a109
+    elif a == 110:
+        return a110
+    elif a == 111:
+        return a111
+    elif a == 112:
+        return a112
+    elif a == 113:
+        return a113
+    elif a == 114:
+        return a114
+    elif a == 115:
+        return a115
+    elif a == 116:
+        return a116
+    elif a == 117:
+        return a117
+    elif a == 118:
+        return a118
+    elif a == 119:
+        return a119
+    elif a == 120:
+        return a120
+    elif a == 121:
+        return a121
+    elif a == 122:
+        return a122
+    elif a == 123:
+        return a123
+    elif a == 124:
+        return a124
+    elif a == 125:
+        return a125
+    elif a == 126:
+        return a126
+    elif a == 127:
+        return a127
+    elif a == 128:
+        return a128
+    elif a == 129:
+        return a129
+    elif a == 130:
+        return a130
+    elif a == 131:
+        return a131
+    elif a == 132:
+        return a132
+    elif a == 133:
+        return a133
+    elif a == 134:
+        return a134
+    elif a == 135:
+        return a135
+    elif a == 136:
+        return a136
+    elif a == 137:
+        return a137
+    elif a == 138:
+        return a138
+    elif a == 139:
+        return a139
+    elif a == 140:
+        return a140
+    elif a == 141:
+        return a141
+    elif a == 142:
+        return a142
+    elif a == 143:
+        return a143
+    elif a == 144:
+        return a144
+    elif a == 145:
+        return a145
+    elif a == 146:
+        return a146
+    elif a == 147:
+        return a147
+    elif a == 148:
+        return a148
+    elif a == 149:
+        return a149
+    elif a == 150:
+        return a150
+    elif a == 151:
+        return a151
+    elif a == 152:
+        return a152
+    elif a == 153:
+        return a153
+    elif a == 154:
+        return a154
+    elif a == 155:
+        return a155
+    elif a == 156:
+        return a156
+    elif a == 157:
+        return a157
+    elif a == 158:
+        return a158
+    elif a == 159:
+        return a159
+    elif a == 160:
+        return a160
+    elif a == 161:
+        return a161
+    elif a == 162:
+        return a162
+    elif a == 163:
+        return a163
+    elif a == 164:
+        return a164
+    elif a == 165:
+        return a165
+    elif a == 166:
+        return a166
+    elif a == 167:
+        return a167
+    elif a == 168:
+        return a168
+    elif a == 169:
+        return a169
+    elif a == 170:
+        return a170
+    elif a == 171:
+        return a171
+    elif a == 172:
+        return a172
+    elif a == 173:
+        return a173
+    elif a == 174:
+        return a174
+    elif a == 175:
+        return a175
+    elif a == 176:
+        return a176
+    elif a == 177:
+        return a177
+    elif a == 178:
+        return a178
+    elif a == 179:
+        return a179
+    elif a == 180:
+        return a180
+    elif a == 181:
+        return a181
+    elif a == 182:
+        return a182
+    elif a == 183:
+        return a183
+    elif a == 184:
+        return a184
+    elif a == 185:
+        return a185
+    elif a == 186:
+        return a186
+    elif a == 187:
+        return a187
+    elif a == 188:
+        return a188
+    elif a == 189:
+        return a189
+    elif a == 190:
+        return a190
+    elif a == 191:
+        return a191
+    elif a == 192:
+        return a192
+    elif a == 193:
+        return a193
+    elif a == 194:
+        return a194
+    elif a == 195:
+        return a195
+    elif a == 196:
+        return a196
+    elif a == 197:
+        return a197
+    elif a == 198:
+        return a198
+    elif a == 199:
+        return a198
+    elif a == 200:
+        return a200
+    elif a == 201:
+        return a201
+    elif a == 202:
+        return a202
+    elif a == 203:
+        return a203
+    elif a == 204:
+        return a204
+    elif a == 205:
+        return a205
+    elif a == 206:
+        return a206
+    elif a == 207:
+        return a207
+    elif a == 208:
+        return a208
+    elif a == 209:
+        return a209
+    elif a == 210:
+        return a210
+    elif a == 211:
+        return a211
+    elif a == 212:
+        return a212
+    elif a == 213:
+        return a213
+    elif a == 214:
+        return a214
+    elif a == 215:
+        return a215
+    elif a == 216:
+        return a216
+    elif a == 217:
+        return a217
+    elif a == 218:
+        return a218
+    elif a == 219:
+        return a219
+    elif a == 220:
+        return a220
+    elif a == 221:
+        return a221
+    elif a == 222:
+        return a222
+    elif a == 223:
+        return a223
+    elif a == 224:
+        return a224
+    elif a == 225:
+        return a225
+    elif a == 226:
+        return a226
+    elif a == 227:
+        return a227
+    elif a == 228:
+        return a228
+    elif a == 229:
+        return a229
+    elif a == 230:
+        return a230
+    elif a == 231:
+        return a231
+    elif a == 232:
+        return a232
+    elif a == 233:
+        return a233
+    elif a == 234:
+        return a234
+    elif a == 235:
+        return a235
+    elif a == 236:
+        return a236
+    elif a == 237:
+        return a237
+    elif a == 238:
+        return a238
+    elif a == 239:
+        return a239
+    elif a == 240:
+        return a240
+    elif a == 241:
+        return a241
+    elif a == 242:
+        return a242
+    elif a == 243:
+        return a243
+    elif a == 244:
+        return a244
+    elif a == 245:
+        return a245
+    elif a == 246:
+        return a246
+    elif a == 247:
+        return a247
+    elif a == 248:
+        return a248
+    elif a == 249:
+        return a249
+    elif a == 250:
+        return a250
+    elif a == 251:
+        return a251
+    elif a == 252:
+        return a252
+    elif a == 253:
+        return a253
+    elif a == 254:
+        return a254
+    elif a == 255:
+        return a255
+    elif a== "sss":
+        return b'SSS\n'
