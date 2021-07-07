f5c1c49dd039b6b94157536a3771d6b5937034be
ilyassrekik747 <43963531+ilyassrekik747@users.noreply.github.com>
2021-07-07 15:40:19 +0400

    Delete Inputs.py

--- Inputs.py
+++
@@ -1,8 +0,0 @@
-def T(m):
-    im_1 = Image.open(m,mode="r")
-
-    width, height = im_1.size
-    print(im_1.mode)
-    B = im_1.tobytes()
-    print(width, height)
-#T(the image name or where it is )
